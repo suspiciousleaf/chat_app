b01f8240c14f8ee84558852976d07edf0d96a083
David <d.f.brooks01@gmail.com>
2024-07-12 20:28:18 +0200

    Added database connection wrapper and functions to build database

---
+++ db_utilities/db_setup.py
@@ -0,0 +1,95 @@
+from psycopg2 import sql
+from db_utilities import connect_to_database
+from dotenv import load_dotenv
+from os import getenv
+
+if getenv("DB_USER") is None:
+
+    load_dotenv()
+
+DB_ROOT_USER = getenv("DB_ROOT_USER")
+DB_ROOT_PASSWORD = getenv("DB_ROOT_PASSWORD")
+DB_NAME = getenv("DB_NAME")
+DB_USER = getenv("DB_USER")
+DB_PASSWORD = getenv("DB_PASSWORD")
+
+
+# Setup database commands
+create_user = sql.SQL("CREATE USER {user} WITH PASSWORD {password}").format(
+    user=sql.Identifier(DB_USER), password=sql.Literal(DB_PASSWORD)
+)
+
+create_database = sql.SQL("CREATE DATABASE {dbname} OWNER {owner}").format(
+    dbname=sql.Identifier(DB_NAME), owner=sql.Identifier(DB_USER)
+)
+
+grant_privileges = sql.SQL(
+    "GRANT ALL PRIVILEGES ON DATABASE {dbname} TO {user}"
+).format(dbname=sql.Identifier(DB_NAME), user=sql.Identifier(DB_USER))
+
+
+@connect_to_database
+def build_database(db, cursor, commands):
+    try:
+        for command in commands:
+            cursor.execute(command)
+
+        db.commit()
+
+        print("Database created successfully.")
+
+    except Exception as error:
+        print(f"Error: {error}")
+        db.rollback()
+
+
+build_database_commands = [create_user, create_database, grant_privileges]
+
+build_database(commands=build_database_commands)
+
+# Create table statements
+create_users_table = """
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+"""
+
+create_messages_table = """
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id),
+    channel VARCHAR(50),
+    content TEXT,
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+);
+"""
+
+
+@connect_to_database
+def create_tables(db, cursor, commands):
+    try:
+        for command in commands:
+            cursor.execute(command)
+
+        db.commit()
+
+        cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+        )
+        results = cursor.fetchall()
+        if cursor:
+            print("The following tables have been created")
+
+        for result in results:
+            print(result)
+
+    except Exception as error:
+        print(f"Error: {error}")
+        db.rollback()
+
+
+create_tables_commands = [create_users_table, create_messages_table]
+
+create_tables(commands=create_tables_commands)
---
+++ db_utilities/db_utilities.py
@@ -0,0 +1,58 @@
+from os import getenv
+from dotenv import load_dotenv
+import psycopg2
+
+# Check of environment variables are loaded, and if not load them from .env. Also check if running locally or not, which changes some of the information.
+
+if getenv("DB_USER") is None:
+
+    load_dotenv()
+
+DB_HOST = getenv("DB_HOST")
+DB_PORT = getenv("DB_PORT")
+DB_ROOT_USER = getenv("DB_ROOT_USER")
+DB_ROOT_PASSWORD = getenv("DB_ROOT_PASSWORD")
+DB_NAME = getenv("DB_NAME")
+DB_USER = getenv("DB_USER")
+DB_PASSWORD = getenv("DB_PASSWORD")
+
+
+class DatabaseConnectionError(Exception):
+    """Custom error that is raised when connecting to the database fails"""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+
+# Decorator function
+def connect_to_database(original_func):
+    """Decorator function to connect to the database and run the function.
+
+    Args:
+        original_func (function): Function to run on the database
+    """
+
+    def make_connection(*args, **kwargs):
+        results = None
+        try:
+            with psycopg2.connect(
+                dbname=DB_NAME,
+                user=DB_ROOT_USER,
+                password=DB_ROOT_PASSWORD,
+                host=DB_HOST,
+                port=DB_PORT,
+            ) as db:
+                with db.cursor() as cursor:
+                    # kwargs for db and cursor to avoid conflicts with 'self'
+                    results = original_func(db=db, cursor=cursor, *args, **kwargs)
+
+        except psycopg2.Error as e:
+            raise DatabaseConnectionError(f"Database connection error: {e}")
+
+        except Exception as e:
+            raise DatabaseConnectionError(f"An unexpected error occurred: {e}")
+
+        return results
+
+    return make_connection
