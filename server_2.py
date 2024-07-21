370ac1fe7d1d45fb746c88b3b728fcb842528523
David <d.f.brooks01@gmail.com>
2024-07-21 17:36:46 +0200

    New server version using connection pool, single websocket per client

---
+++ server_2.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: dict[str:WebSocket] = {}
+
+    async def connect(self, websocket: WebSocket, client_id: str):
+        # TODO Use GUI login to provide bearer token to client, use this to authorize the websocket connection and add logic here. Store tokens in Redis.
+        await websocket.accept()
+        self.active_connections[client_id] = websocket
+
+    def disconnect(self, client_id: str):
+        # TODO Could use this to send a 'user disconnected' message to the channel
+        self.active_connections.pop(client_id, None)
+
+    async def send_message(self, message: str, client_id: str):
+        websocket = self.active_connections.get(client_id)
+        if websocket:
+            await websocket.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+# TODO Move client_id and bearer token to header
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket, client_id)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Handle incoming messages
+
+    except WebSocketDisconnect:
+        manager.disconnect(client_id)
+
+
+#! Check FastAPI websockets docs https://fastapi.tiangolo.com/advanced/websockets/
