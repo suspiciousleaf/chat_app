054947da1ee804b836f7b6868cbdaa9cbe977520
David <d.f.brooks01@gmail.com>
2024-08-02 22:07:59 +0200

    Changed from Customer tkinter to Themed tkinter

--- client/main_chat.py
+++ client/main_chat.py
@@ -1,6 +1,10 @@
 import datetime
 import tkinter as tk
-import customtkinter as ctk
+from tkinter import ttk
+
+from networking.connect_websocket import MyWebSocket
+
+# import customtkinter as ctk
 
 LARGE_FONT_STYLE = ("Arial", 40, "bold")
 SMALL_FONT_STYLE = ("Arial", 16)
@@ -16,15 +20,17 @@ LABEL_COLOUR = "#25265E"
 
 class Chattr:
     def __init__(self):
-        self.window = ctk.CTk()
+        self.window = tk.Tk()
         self.width = 375
         self.height = 375
         self.window.geometry(f"{self.width}x{self.height}")
-        self.window.resizable(0, 0)
+        # self.window.resizable(0, 0)
         self.window.title("Chattr")
         self.buttons = {}
         self.labels = {}
         self.entries = {}
+        self.username = "username_1"
+        self.client_websocket = self.connect_client_websocket()
         # self.frame = self.create_display_frame()
         self.username = tk.StringVar(value="username")
         self.password = tk.StringVar(value="password")
@@ -32,22 +38,27 @@ class Chattr:
         self.screen_text = tk.StringVar(value="Messages will appear here")
         self.create_text_field()
         self.create_text_entry()
+        self.configure_responsive()
+
+    def connect_client_websocket(self):
+        return MyWebSocket(self.username)
 
     def create_text_field(self):
         self.text_field = tk.Text(
             self.window,
             width=self.width,
             # height=200,  # Set height to a smaller number of lines
-            bg=OFF_WHITE,
+            background=OFF_WHITE,
             state="disabled",
             wrap="word",
         )
         self.text_field.grid(row=0, column=0, columnspan=2, sticky="nsew")
 
-        # # Configure grid row and column weights
-        self.window.grid_rowconfigure(0, weight=1)
-        self.window.grid_columnconfigure(0, weight=1)
-        self.window.grid_columnconfigure(1, weight=0)
+    def configure_responsive(self):
+        # Configure grid row and column weights for responsive behaviour
+        self.window.rowconfigure(0, weight=1)
+        self.window.columnconfigure(0, weight=1)
+        self.window.columnconfigure(1, weight=0)
 
     def send_message(self, event=None):
         message = self.entries["write_message"].get()
@@ -60,16 +71,16 @@ class Chattr:
             self.entries["write_message"].delete(0, tk.END)
 
     def create_text_entry(self):
-        message_entry = ctk.CTkEntry(
-            self.window, width=self.width - 70, bg_color=OFF_WHITE
+        message_entry = ttk.Entry(
+            self.window, width=self.width - 70, background=OFF_WHITE
         )
         message_entry.grid(row=1, column=0, sticky="ew")
         message_entry.bind("<Return>", self.send_message)
         self.entries["write_message"] = message_entry
 
-        send_button = ctk.CTkButton(
-            self.window, width=50, height=20, text="Send", command=self.send_message
-        )
+        send_button = ttk.Button(
+            self.window, text="Send", command=self.send_message
+        )  # width=50, height=20,
         send_button.grid(row=1, column=1)
         self.buttons["send"] = send_button
 
@@ -98,8 +109,8 @@ class Chattr:
         self.delete_entries()
 
     def create_display_frame(self):
-        frame = ctk.CTkFrame(
-            self.window, height=221, bg_color="white", fg_color="white"
+        frame = ttk.Frame(
+            self.window, height=221, background="white", foreground="white"
         )
         frame.pack(expand=True, fill="both")
         return frame
