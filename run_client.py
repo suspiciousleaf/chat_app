c39d252da2cf80417fb8d61bd29f76d8f9e95508
David <d.f.brooks01@gmail.com>
2024-08-10 22:11:33 +0200

    Updated TODO items

--- run_client.py
+++ run_client.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     chattr.run()
 
 #! Client
-# TODO On startup, add server status request response to the screen
-# TODO Disable buttons if server status is not ok
 # TODO Make logout button
 # TODO Filter messages by channel
 # TODO Make buttons remain still between screens during login
@@ -17,13 +15,11 @@ if __name__ == "__main__":
 
 #! Server
 # TODO Add timeout for batch message inserts
-# TODO Make endpoint to check server health, and make server verify connection to database and redis before responding.
 # TODO Check ws login event loop thread logic and behaviour
 # TODO Add message timestamp
 # TODO Add ability for people to update their channel subscriptions via endpoint
 # TODO Auth is using its own instance of db_manager, refactor to pass through the main instance
-# TODO Listener isn't listening since reorganising file structure
-
+# TODO check disconnect logic
 
 #! Database
 # TODO Add password salt to database
