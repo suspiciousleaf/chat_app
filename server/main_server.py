ea51987ce5ede2d4d56753b4493ab0be6a920ac7
David <d.f.brooks01@gmail.com>
2024-08-10 13:59:42 +0200

    Deleted connection_manager and redis_manager code moved to separate file

--- server/main_server.py
+++ server/main_server.py
@@ -7,10 +7,8 @@ from fastapi import (
     Depends,
 )
 from pydantic import BaseModel, Field
-from redis.exceptions import ConnectionError, TimeoutError
 from contextlib import asynccontextmanager
 import json
-import redis
 import time
 import asyncio
 from pathlib import Path
@@ -35,27 +33,22 @@ env_path = Path(".") / ".env"
 if env_path.exists():
     load_dotenv(env_path)
 
-# REDIS_URL = "redis://localhost:6379/0"
-# REDIS_QUEUE = "chat_queue"
-# MAX_RECONNECT_ATTEMPTS = 5
-# RECONNECT_DELAY = 5  # seconds
-
 
 # Endpoint to ping server
 # TODO Add server diagnostics, eg confirm database and redis connections are live, and report back here
 @app.get("/", status_code=status.HTTP_200_OK)
-def ping():
-    db_status = connection_man.db.verify_connection_and_tables()
-    redis_status = connection_man.redis_man.verify_connection()
+async def ping():
+    db_status = await connection_man.db.verify_connection_and_tables()
+    redis_status = await connection_man.redis_man.verify_connection()
     if db_status.get("status") and redis_status.get("status"):
         return {"status": "ready"}
-    else:
-        statuses = []
-        if db_status.get("status") == False:
-            statuses.append(db_status.get("detail"))
-        if redis_status.get("status") == False:
-            statuses.append(redis_status.get("detail"))
-        return {"status": ", ".join(statuses)}
+
+    error_details = []
+    if not db_status["status"]:
+        error_details.append(db_status["detail"])
+    if not redis_status["status"]:
+        error_details.append(redis_status["detail"])
+    return {"status": ", ".join(error_details)}
 
 
 @app.get("/tables")
@@ -70,131 +63,6 @@ def get_filepath():
     return db.read_db_filepath()
 
 
-# class RedisManager:
-#     def __init__(self):
-#         self.redis = redis.Redis.from_url(REDIS_URL, decode_responses=True)
-
-#     async def enqueue_message(self, message: dict):
-#         message_as_string = json.dumps(message)
-#         await asyncio.to_thread(self.redis.rpush, REDIS_QUEUE, message_as_string)
-#         # await self.redis.rpush(REDIS_QUEUE, message)
-
-#     async def dequeue_message(self) -> str:
-#         return await asyncio.to_thread(self.redis.blpop, REDIS_QUEUE, timeout=0)
-#         # return await self.redis.blpop(REDIS_QUEUE, message)
-
-#     async def get_len(self, queue: str) -> int:
-#         return await asyncio.to_thread(self.redis.llen, queue)
-
-#     async def verify_connection(self) -> dict:
-#         """Verify if the Redis connection is working."""
-#         try:
-#             ping_result = await asyncio.to_thread(self.redis.ping)
-#             if ping_result:
-#                 return {"status": "ok", "details": None}
-#         except redis.RedisError:
-#             return {"status": "error", "details": "redis connection failed"}
-
-
-# class ConnectionManager:
-#     def __init__(self):
-#         # TODO Add function to check length of self.message_store. If more than X messages, or more than Y time, upload messages to database in a batch, reset timer, and clear list.
-#         self.active_connections: dict[str, dict[WebSocket, set]] = {}
-#         self.redis_man = RedisManager()
-#         # self.user_channels: dict[str:list[str]] = {}
-#         self.listener_task = None
-#         self.message_store: list = []
-#         self.time_since_message_backup: float = time.time()
-
-#     async def connect(self, websocket: WebSocket, username: str):
-#         await websocket.accept()
-#         # TODO Use GUI login to provide bearer token to client, use this to authorize the websocket connection and add logic here. Store tokens in Redis.
-#         # print(f"Websocket connection requested for user: {username}")
-#         #! channels: set = retrieve_channels(username=username)
-#         channels = db.retrieve_channels(username)
-#         channels.add("welcome")
-#         print(f"User is a member of {channels} channels")
-#         # Add all users to the "welcome" channel
-#         self.active_connections[username] = {"ws": websocket, "channels": channels}
-#         # self.active_connections[username] = websocket
-#         # self.user_channels[username] = await self.get_user_channels(username)
-
-#         # Starts the redis listener once at least one user is connected.
-#         if not self.listener_task or self.listener_task.done():
-#             self.listener_task = asyncio.create_task(self.start_listener())
-#             print("First connection opened, starting listener")
-
-#     async def disconnect(self, username: str):
-#         # self.active_connections.pop(username, None)
-#         # self.user_channels.pop(username, None)
-#         # TODO Could use this to send a 'user disconnected' message to the channel
-#         # connection = self.active_connections.get(username)
-#         # if connection["ws"]:
-#         #     await connection["ws"].close()
-#         self.active_connections.pop(username, None)
-#         # print(f"Disconnected user: {username}")
-
-#         # Disable listener if there are no active connections
-#         if not self.active_connections and self.listener_task:
-#             self.listener_task.cancel()
-#             self.listener_task = None
-#             print("No active connections, stopping listener.")
-
-#     # async def disconnect(self, username: str):
-#     #     connection = self.active_connections.pop(username, None)
-#     #     if connection:
-#     #         await connection["ws"].close()
-#     #     if not self.active_connections and self.listener_task:
-#     #         self.listener_task.cancel()
-#     #         try:
-#     #             await self.listener_task
-#     #         except asyncio.CancelledError:
-#     #             pass
-#     #         self.listener_task = None
-#     #         print("No active connections, stopping listener.")
-
-#     async def broadcast(self, message: dict):
-#         channel = message.get("channel")
-#         # print(f"Channel as seen by broadcast: {channel}")
-#         for username, user_connection in self.active_connections.items():
-#             if channel in user_connection["channels"]:
-#                 # print(f"Sending {message} to {username}")
-#                 await user_connection["ws"].send_text(json.dumps(message))
-
-#     async def listen_for_messages(self):
-#         # If no messages in queue, wait 0.25 seconds and then check again. As long as there are messages in the queue, send them out as fast as possible.
-#         while True:
-#             try:
-#                 while await self.redis_man.get_len(queue=REDIS_QUEUE) > 0:
-#                     try:
-#                         _, message = await self.redis_man.dequeue_message()
-#                         if message:
-#                             data = json.loads(message)
-#                             #! print(f"message as received by message_listener: {data}")
-#                             await self.broadcast(data)
-#                     except json.JSONDecodeError:
-#                         print(f"Failed to parse message data: {message}")
-#                 await asyncio.sleep(0.25)
-
-#             except Exception as e:
-#                 print(f"Error in message listener: {str(e)}")
-#                 await asyncio.sleep(1)
-
-#     async def start_listener(self):
-#         attempts = 0
-#         while attempts < MAX_RECONNECT_ATTEMPTS:
-#             try:
-#                 await self.listen_for_messages()
-#             except (ConnectionError, TimeoutError) as e:
-#                 print(f"Redis connection error: {str(e)}. Attempting to reconnect...")
-#                 attempts += 1
-#                 await asyncio.sleep(RECONNECT_DELAY)
-#             except Exception as e:
-#                 print(f"Unexpected error in listener: {str(e)}")
-#                 break
-#         print("Max reconnection attempts reached. Listener stopped.")
-
-
 connection_man = ConnectionManager(db)
 
 
@@ -206,7 +74,6 @@ class AccountCreate(BaseModel):
 # Endpoint to create an account
 @app.post("/create_account", status_code=status.HTTP_201_CREATED)
 async def create_account_endpoint(account: AccountCreate):
-    # return create_account(account.username, account.password)
     return db.create_account(account.username, account.password)
 
 
