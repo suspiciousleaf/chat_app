373e001cb7348631c51547fd04de514cedc28c94
David <d.f.brooks01@gmail.com>
2024-08-17 21:55:25 +0200

    Updated todo items

--- run_client.py
+++ run_client.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
 # TODO Make buttons remain still between screens during login
 # TODO Store channel message text in a dict so it can persist on channel updates when tabs are destroyed and rebuilt
 # TODO show_context_menu() needs a try/except block when not clicking on a tab, find out why
-# TODO Maybe open a PR to fix the issue of .index returning "" and causing TclError? Should return -1 if clicking outside of a tab, not through an exception
-# TODO Change channel sub behaviour so adding or removing a channel sends a formatted ws message, which updates db and then sends a formatted message back that causes the channels to update
+# TODO Make sure username isn't already subscribed to a channel when adding a new one
 
 
 #! Auth
@@ -18,17 +17,12 @@ if __name__ == "__main__":
 
 #! Server
 # TODO Check ws login event loop thread logic and behaviour
-# TODO Add ability for people to update their channel subscriptions via endpoint
 # TODO Auth is using its own instance of db_manager, refactor to pass through the main instance
 # TODO check disconnect logic
-# TODO Parse received messages for add / leave channel
 
 #! Database
 # TODO Add password salt to database
-# TODO Add database functions to update channel subscriptions
 # TODO Make db_manager async
-# TODO DB method to add channel
-# TODO DB method to leave channel
 
 #! General
 # TODO Add proper logging
@@ -37,3 +31,6 @@ if __name__ == "__main__":
 # TODO End-to-end testing
 # TODO User flow testing
 # TODO Perf testing
+
+#! Other
+# TODO Maybe open a PR to fix the issue of .index returning "" and causing TclError? Should return -1 if clicking outside of a tab, not through an exception
