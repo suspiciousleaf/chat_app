73546c8febd32539b4c3c67f4e357667f1a76d0e
David <d.f.brooks01@gmail.com>
2024-08-31 22:25:36 +0200

    Added function to convert to .csv

--- load_testing/create_accounts.py
+++ load_testing/create_accounts.py
@@ -4,6 +4,7 @@ import json
 import random
 import time
 from os import getenv
+import csv
 
 from dotenv import load_dotenv
 from sample_words import sample_words
@@ -11,7 +12,9 @@ from sample_words import sample_words
 load_dotenv()
 
 URL = getenv("URL")
+# URL = "http://127.0.0.1:8000"
 WS_URL = getenv("WS_URL")
+# WS_URL = "ws://127.0.0.1:8000"
 LOGIN_ENDPOINT = "/auth/token"
 CREATE_ACCOUNT_ENDPOINT = "/create_account"
 
@@ -32,14 +35,14 @@ def create_accounts_local(num: int):
         f.write(json.dumps(accounts))
 
 
-def create_account_on_server(username: str, password: str):
+def create_account_on_server(s: requests.Session, username: str, password: str):
     """Create an account on the server with the provided credentials"""
     account_info = {
         "username": username,
         "password": password,
     }
 
-    response = requests.post(f"{URL}{CREATE_ACCOUNT_ENDPOINT}", json=account_info)
+    response = s.post(f"{URL}{CREATE_ACCOUNT_ENDPOINT}", json=account_info)
     response.raise_for_status()
 
 
@@ -47,15 +50,27 @@ def create_accounts_on_server_from_local_file():
     """Load the local accounts file and create those accounts on the server"""
     with open("load_testing/accounts.json", "r") as f:
         accounts = json.load(f)
+    s = requests.Session()
     i = 0
     for username, password in accounts.items():
         try:
-            create_account_on_server(username, password)
+            create_account_on_server(s, username, password)
         except Exception as e:
             print(f"{username=}, {password=}, {e}")
         i += 1
         if not i % 50:
             print(f"Account {i} processed")
+    print(f"{len(accounts)} accounts created")
+
+
+def convert_accounts_json_to_csv():
+    with open("load_testing/accounts.json", "r") as f:
+        accounts = json.load(f)
+
+    with open("load_testing/accounts.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(("username", "password"))
+        writer.writerows(accounts.items())
 
 
 # create_accounts_on_server_from_local_file()
