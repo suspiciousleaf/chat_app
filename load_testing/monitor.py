722a0a4bc1652006d4e8559ea8c92e0a285db8a7
David <d.f.brooks01@gmail.com>
2024-09-18 21:15:48 +0200

    Increased data points retrieved

--- load_testing/monitor.py
+++ load_testing/monitor.py
@@ -8,12 +8,11 @@ from logging import Logger
 from load_testing.virtual_user import User
 
 class Monitor(User):
-    def __init__(self, logger, bearer_token: str, username: str = None, password: str = None,):
-        super().__init__(bearer_token, username=username, password=password)
-        self.logger: Logger = logger
-        self.perf_data: dict = {channel:{} for channel in self.test_channels}
+    def __init__(self, logger: Logger, account: dict):
+        super().__init__(logger, account)
+        self.perf_data: dict = {} #{channel:{} for channel in self.test_channels}
         self.perf_test_id = 0
-        logger.info("Monitor created")
+        self.logger.info(f"Created: {self}")
         
         # Info to test/get from server:
         # message ping time
@@ -23,23 +22,24 @@ class Monitor(User):
         # Subscribers to that channel - ensure ping reply is sent to monitor after all other users in channel
         
     async def start_activity(self):
-        self.logger.info("Monitor activity starting")
+        self.logger.info("Monitor.start_activity()")
         # Join all channels, send message every X seconds, time how long until it returns. Maybe also join and leave channels and see how long it takes. Use a while True loop rather than X actions to ensure it is live throughout the entire test.
         # await self.match_test_channel_subscriptions()
         while self.connection_active:
             # for channel in self.channels:
-            await self.send_perf_ping() #channel)
-            await asyncio.sleep(1)
+            try:
+                await self.send_perf_ping() #channel)
+                await asyncio.sleep(1)
+            except asyncio.CancelledError:
+                break
 
     async def send_perf_ping(self): #, channel):
         """Send a message to the server with a request for server stats, and to measure response times"""
-        self.logger.info(f"Sending {self.perf_test_id=}")
         message = {
                 "event": "perf_test",
-                # "channel": channel,
                 "perf_test_id": self.perf_test_id
             }
-        self.logger.info(message)
+        self.logger.debug(f"Sending: {message}")
         self.perf_data[self.perf_test_id] = {"latency": time.perf_counter()}
         await self.client_websocket.send_message(message)
         self.perf_test_id += 1
@@ -47,37 +47,45 @@ class Monitor(User):
 
     async def handle_perf_response(self, message: dict):
         """Handle the perf test response message and log the data"""
+        self.logger.debug(f"Monitor received: {message}")
         perf_test_id = message.get("perf_test_id")
         time_sent_at = self.perf_data[perf_test_id].get("latency")
         self.perf_data[perf_test_id] = {
             "perf_test_id" : perf_test_id,
-            "latency": time.perf_counter() - time_sent_at,
-            "server_cpu_load" : message.get("server_cpu_load"),
-            "server_memory_usage" : message.get("server_memory_usage"),
-            "total_active_connections" : message.get("total_active_connections"),
-            # "channel_active_connections" : message.get("channel_active_connections"),
+            "latency": round(time.perf_counter() - time_sent_at, 4),
+            "cpu_load" : message.get("cpu_load"),
+            "memory_usage" : message.get("memory_usage"),
+            "active_connections" : message.get("active_connections") - 1,
+            "message_volume": message.get("message_volume"),
+            "mv_period": message.get("mv_period"),
+            "mv_adjusted": message.get("mv_adjusted"),
             }
-        self.logger.info(self.perf_data[perf_test_id])
+        self.logger.debug(f"{self.perf_data[perf_test_id]=}")
+        if message.get("active_connections") < 1:
+            await self.logout()
 
 
     async def listen_for_messages(self):
         """Listen for perf test response messages"""
-        self.logger.info("Listening for messages")
+        self.logger.debug("Monitor listening for messages")
         while self.connection_active:
             try:
-                message_str = await self.client_websocket.websocket.recv()
-                message: dict = json.loads(message_str)
-                if message is not None:
+                # message_str = await self.client_websocket.websocket.recv()
+                message_str = await self.client_websocket.receive_message()
+                self.logger.debug(f"Monitor received: {message_str=}")
+                if message_str is not None:
+                    message: dict = json.loads(message_str)
                     event_type = message.get("event")
                     if event_type == "perf_test":
                         await self.handle_perf_response(message)
-            except asyncio.CancelledError:
-                break  
+            except asyncio.CancelledError as e:
+                pass 
             except Exception as e:
                 if self.connection_active:
-                    self.logger.warning(f"Error receiving message: {e}", exc_info=True)
+                    self.logger.warning(f"Monitor listener Exception: {self.connection_active=}, {e}", exc_info=True)
                     # traceback.print_tb(e.__traceback__)
                 else:
+                    self.logger.warning(f"Monitor listener Exception: {self.connection_active=}, {e}", exc_info=True)
                     break 
 
     async def match_test_channel_subscriptions(self):
@@ -90,5 +98,5 @@ class Monitor(User):
 
 
     def __repr__(self):
-        return f"Monitor({self.bearer_token=}, {self.actions=}, {self.test_channels=}, {self.username=}, {self.password=})"
+        return f"Monitor({self.username=})"
 
