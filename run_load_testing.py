d6aed5bfd46514093d0955d0c20af72537557c29
David <d.f.brooks01@gmail.com>
2024-11-09 15:53:31 +0100

    Added progress bar

--- run_load_testing.py
+++ run_load_testing.py
@@ -1,18 +1,24 @@
+import time
+import threading
+
+from tqdm import trange
+
 import logging
 from load_testing.load_tester import LoadTester
 
 
-NUMBER_OF_TEST_ACCOUNTS = 225
+NUM_ACCOUNTS = 425
 
 NUM_ACTIONS = 40
 
-CONNECTION_DELAY = 0.25 
-# CONNECTION_DELAY = None
+CONNECTION_DELAY = 0.35 
+# CONNECTION_DELAY = 0
 
-DELAY_BEFORE_ACTION = NUMBER_OF_TEST_ACCOUNTS * CONNECTION_DELAY
+DELAY_BEFORE_ACTION = NUM_ACCOUNTS * CONNECTION_DELAY
+# DELAY_BEFORE_ACTION = 0
 DELAY_BETWEEN_ACTIONS = 6
 
-run_time = DELAY_BEFORE_ACTION + (NUM_ACTIONS * DELAY_BETWEEN_ACTIONS)
+run_time = int(DELAY_BEFORE_ACTION + (NUM_ACTIONS * DELAY_BETWEEN_ACTIONS) + (NUM_ACCOUNTS * CONNECTION_DELAY))
 
 logging.getLogger("asyncio").setLevel(logging.CRITICAL)
 logger = logging.getLogger('LoadTester')
@@ -23,7 +29,15 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
-load_tester = LoadTester(logger, NUMBER_OF_TEST_ACCOUNTS, NUM_ACTIONS, DELAY_BEFORE_ACTION, DELAY_BETWEEN_ACTIONS, CONNECTION_DELAY)
-load_tester.start()
+load_tester = LoadTester(logger, NUM_ACCOUNTS, NUM_ACTIONS, DELAY_BEFORE_ACTION, DELAY_BETWEEN_ACTIONS, CONNECTION_DELAY)
 
-# Not happy at 500, test lower
+def progress_bar():
+    """Function to display a progress bar"""
+    for _ in trange(run_time, desc="Load Test Progress", unit="s"):
+        time.sleep(1) 
+
+# Run progress bar in a separate thread
+progress_bar_thread = threading.Thread(target=progress_bar)
+progress_bar_thread.start()
+
+load_tester.start()
