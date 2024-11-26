a3c9b8f28d201876609d61e51811e1ad2804d7b9
David <d.f.brooks01@gmail.com>
2024-11-26 21:59:13 +0100

    Updated TODO

--- run_client.py
+++ run_client.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     chattr.run()
 
 #! Client
-# TODO Make buttons remain still between screens during login
-# TODO add full channel name banner to top of client, or show full name on mouse hover
 # TODO Print all message history in one block, rather than line by line
 # TODO Add a max message character limit
 
@@ -14,25 +12,16 @@ if __name__ == "__main__":
 # TODO Verify token expiry is working as expected
 
 #! Server
-# TODO Read DDIA on message encoding and decoding, protobuff. Could send messages faster over ws
 # TODO Button or trigger to load older message history?
 # TODO Add a max message character limit
-# Errors sending messages to websockets that have disconnected, still present in docker logs
-#! Message history: Size count is probably broken by using orjson or protobuf
+#! Message history: Size count is broken by using orjson or protobuf
 
 
 #! Database
 
-
 #! General
-# TODO Add proper logging
-# TODO unit testing
-# TODO Integration testing
-# TODO End-to-end testing
-# TODO User flow testing
-
+# TODO add testing
 
 #! Load testing
-# TODO Add flame graph profiling to server
-# ? Create endpoint to delete account, so accounts can be created, used, and deleted dynamically during testing
+
 
