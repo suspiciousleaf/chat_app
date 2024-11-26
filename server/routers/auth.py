42798407cf674ca8768f77c3645c834871245c93
David <d.f.brooks01@gmail.com>
2024-11-26 18:35:03 +0100

    Removed unused print statement

--- server/routers/auth.py
+++ server/routers/auth.py
@@ -232,7 +232,6 @@ async def login_for_access_token(
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
-    # print(f"auth.py Authorized user: {user.username}")
     return {"access_token": access_token, "token_type": "bearer"}
 
 
